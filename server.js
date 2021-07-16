d381fff9a90fc63812b991d86d4712d64269f854
Sam Morgen <samorgen14@gmail.com>
2021-07-15 18:36:17 -0700

    Added connection to database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,23 @@ const app = express();
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//connect to the database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    //MYSQL username,
+    user: "root",
+    //MYSQL password
+    password: "Rt@n2010",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
+
+db.query("SELECT * FROM candidates", (err, rows) => {
+  console.log(rows);
+});
 
 app.use((req, res) => {
   res.status(404).end();
